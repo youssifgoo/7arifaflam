cb76b8db1d23a31cce92dea650ba2452b1134916
YOUSSIF GOO <youssifgoo45@gmail.com>
2025-04-11 23:58:30 +0200

    save aflam play 1

---
+++ actor.js
@@ -0,0 +1,171 @@
+let actors = [
+    {img: `actors/ahmedHelmi.png`,
+    film: ["Aasef3alaElez3ag", "assalEsoed", "elNazer", "ga3latnyMogrman", "MedoMashakel", "lafWeDaoaran", "kedaReda", "SayeBahr", "xLarg", "zakiShan", "MatabSinai", "zarfTarek", "55Esaaf"]
+    },
+    {img: `actors/ahmedMekky.png`,
+    film: ["laTrago3WlaEstslam", "samerAboElNile", "sima3liBaba"]
+    },
+   {img: `actors/7asan7osny.png`,
+    film: ["3okal", "a7lamElfataElataaesh", "aeathono", "AskarFiElMu3askar", "bo7a", "ElBashaTelmeez", "elLimbi", "elNazer", "ga3latnyMogrman", "Ghapy", "H-daboor", "MedoMashakel", "YaAnaYaKhalty", "zakiShan", "7a7aWeTofa7a", "ElLemby8Giga", "EyalHabiba", "kabtenMasr", "Karkar", "ShebhMonharef", "samerWeShaherWeBaher", "a7laElAuqat", "Lakhmetrass", "3eshElZaogia", "katkot", "ka7imFeElHend"]
+    },
+    {img: `actors/ahmedELsaka.png`,
+    film: ["IbrahimElAbyad", "TaimourWeShafika", "s3ediFeElGam3aElamirkia", "ElMaslaha", "AlGezira", "7arbItalya", "3alaGanbYasta", "3anEl3eahkWeElHawa", "EbnElQonsol"]
+    },
+    {img: `actors/doniaSamerGhanem.png`,
+    film: ["lafWeDaoaran", "laTrago3WlaEstslam", "YaAnaYaKhalty", "xLarg", "365YomSa3ada"]
+    },
+    {img: `actors/hendSabry.png`,
+    film: ["IbrahimElAbyad", "elFeelElAzrak", "AlGezira", "a7laElAuqat"]
+    },
+    {img: `actors/lotfiLabib.png`,
+    film: ["assalEsoed", "ElBashaTelmeez", "ElTalataYeshtghalonha", "H-daboor", "kedaReda", "YaAnaYaKhalty", "SayeBahr", "FeMahatetMasr", "Karkar", "tabakhElRais", "SHIKAMARA", "amirElBi7ar", "ramadanMabrok", "7a7aWeTofa7a", "elLimbi", "SayedElAtefy", "3andlibElDuki", "365YomSa3ada", "aboShanab", "gaanaElBaianElTali", "Lakhmetrass", "sima3liBaba", "HasalKher", "SahibSahbu", "HarameyaFeThailand"]
+    },
+    {img: `actors/mohammedHenedy.png`,
+    film: ["AskarFiElMu3askar", "TetaRaheba", "YaAnaYaKhalty", "amirElBi7ar", "folElSinElAzim", "Karkar", "ramadanMabrok", "WeshEgram", "3andlibElDuki", "gaanaElBaianElTali", "s3ediFeElGam3aElamirkia", "SahibSahbu"]
+    },
+    {img: `actors/mohammedSa3d.png`,
+    film: ["3okal", "bo7a", "Boshkash", "elNazer", "elLimbi", "ElLemby8Giga", "Karkar", "55Esaaf", "katkot"]
+    },
+    {img: `actors/monaZaki.png`,
+    film: ["TaimourWeShafika", "s3ediFeElGam3aElamirkia", "KhaltyFaransa", "AbuAli", "elDadaDody", "WeladEl3m", "3anEl3eahkWeElHawa"]
+    },
+    {img: `actors/nelliKarem.png`,
+    film: ["a7lamElfataElataaesh", "Ghapy", "elFeelElAzrak", "7arbItalya"]
+    },
+    {img: `actors/yassminAbdElaziz.png`,
+    film: ["ElTalataYeshtghalonha", "SayeBahr", "zakiShan", "Karkar", "elrahina", "elDadaDody", "7a7aWeTofa7a", "3esabaDr.Omar", "7aremKarem", "aboShanab", "tumnTastaAshrar"]
+    },
+    {img: `actors/3ablaKamel.png`,
+    film: ["elLimbi", "KhaltyFaransa", "SayedElAtefy", "Karkar", "3awdaElNadla"]
+    },
+    {img: `actors/3alaaMorsi.png`,
+    film: ["YaAnaYaKhalty", "zakiShan", "MedoMashakel", "el7arbEl3alamiaElTalta", "3okal", "samerAboElNile", "Ma3leshEhnaBenetbahdel", "3andlibElDuki", "eo3aOeshk", "3alaGanbYasta", "AskarFiElMu3askar", "Karkar"]
+    },
+    {img: `actors/3ezatAbo3of.png`,
+    film: ["aeathono", "Boshkash", "laTrago3WlaEstslam", "OmarWeSalma2", "ramadanMabrok", "AlGezira", "3awdaElNadla", "MatabSinai"]
+    },
+    {img: `actors/ahmedEzz.png`,
+    film: ["elrahina", "ElMaslaha", "elshab7", "weladRezk", "365YomSa3ada", "HelmAziz"]
+    },
+    {img: `actors/ahmedFat7y.png`,
+    film: ["kabtenMasr", "el7arbEl3alamiaElTalta", "3endamaYak3ElAnsan", "BanatEl3am", "bankEl7az", "ItlauliBarrah", "kalbBalady", "ka7imFeElHend"]
+    },
+    {img: `actors/amrAbdElgilel.png`,
+    film: ["ziko", "SarkhetNamla", "KalemnyShokran", "3andlibElDuki"]
+    },
+    {img: `actors/basemSamra.png`,
+    film: ["weladRezk", "TetaRaheba", "zakiShan", "IbrahimElAbyad", "AlGezira", "3amohm", "elshab7"]
+    },
+    {img: `actors/edward.png`,
+    film: ["a7lamElfataElataaesh", "assalEsoed", "BanatEl3am", "elDadaDody", "FeMahatetMasr", "ga3latnyMogrman", "kabtenMasr", "ramadanMabrok", "ShebhMonharef", "SHIKAMARA", "Boshkash", "3esabaDr.Omar", "7aremKarem", "3eshElZaogia"]
+    },
+    {img: `actors/karemAbdElaziz.png`,
+    film: ["AbuAli", "FeMahatetMasr", "ElBashaTelmeez", "elFeelElAzrak", "faselWaNa3od", "HarameyaFeKG2", "HarameyaFeThailand", "WeladEl3m"]
+    },
+    {img: `actors/khaledElsawy.png`,
+    film: ["zarfTarek", "kedaReda", "elFeelElAzrak", "ElBashaTelmeez", "el3yalHerbt", "AlGezira", "AbuAli", "ItlauliBarrah"]
+    },
+    {img: `actors/magedElkedwani.png`,
+    film: ["AskarFiElMu3askar", "el3yalHerbt", "laTrago3WlaEstslam", "wakfetRekala", "elrahina", "SHIKAMARA", "Sa3aWaNos", "ElLemby8Giga", "faselWaNa3od", "HarameyaFeThailand", "HarameyaFeKG2", "sima3liBaba", "3alaGanbYasta", "AboNasab"]
+    },
+    {img: `actors/maiEzzEldin.png`,
+    film: ["aeathono", "bo7a", "OmarWeSalma2", "SHIKAMARA", "ElLemby8Giga"]
+    },
+    {img: `actors/minnaShalaby.png`,
+    film: ["ziko", "Nour3iney", "kedaReda", "FeMahatetMasr", "FilmHendi", "aboElAraby", "Aasef3alaElez3ag", "samerAboElNile", "a7laElAuqat", "3anEl3eahkWeElHawa", "HelmAziz", "eo3aOeshk"]
+    },
+    {img: `actors/mohammedFarag.png`,
+    film: ["TetaRaheba", "elrahina", "ElMaslaha", "faselWaNa3od"]
+    },
+    {img: `actors/mohammedLotfi.png`,
+    film: ["3amohm", "samerAboElNile", "weladRezk", "EyalHabiba", "ElBashaTelmeez", "MedoMashakel", "ElTalataYeshtghalonha", "3esabaDr.Omar", "faselWaNa3od", "AboNasab"]
+    },
+    {img: `actors/mohammedSalam.png`,
+    film: ["3amohm", "kabtenMasr", "laTrago3WlaEstslam", "elrahina", "sima3liBaba", "akherDikFeMasr", "kalbBalady", "lilaHanaWeSoror", "ka7imFeElHend", "wakfetRekala"]
+    },
+    {img: `actors/mohammedSaroat.png`,
+    film: ["3amohm", "3endamaYak3ElAnsan", "bankEl7az", "el7arbEl3alamiaElTalta", "KalemnyShokran", "WeladEl3m", "AboNasab", "akherDikFeMasr", "kalbBalady", "lilaHanaWeSoror", "ka7imFeElHend", "sab3ElBromba"]
+    },
+    {img: `actors/mohammedMamdo7.png`,
+    film: ["bankEl7az", "wakfetRekala", "IbrahimElAbyad", "elFeelElAzrak", "weladRezk"]
+    },
+    {img: `actors/nour.png`,
+    film: ["3okal", "elrahina", "SayedElAtefy", "MatabSinai", "zarfTarek"]
+    },
+    {img: `actors/sa3edTarablk.png`,
+    film: ["WeshEgram", "tabakhElRais", "folElSinElAzim", "MedoMashakel", "Ghapy", "folElSinElAzim", "ShebhMonharef", "assalEsoed", "kedaReda", "xLarg", "SayeBahr", "Boshkash", "FeMahatetMasr", "el3yalHerbt", "EyalHabiba", "AbuAli", "samerAboElNile", "KhaltyFaransa", "HarameyaFeKG2", "Lakhmetrass"]
+    },
+    {img: `actors/salahAbdallah.png`,
+    film: ["elrahina", "elDadaDody", "zakiShan", "aboElAraby", "AskarFiElMu3askar", "ElMaslaha", "ElTalataYeshtghalonha", "elshab7", "BanatEl3am", "FilmHendi", "365YomSa3ada", "el7arbEl3alamiaElTalta", "AboNasab", "HelmAziz", "LeesBaghdad"]
+    },
+    {img: `actors/shiko.png`,
+    film: ["samerWeShaherWeBaher", "el7arbEl3alamiaElTalta", "BanatEl3am"]
+    },
+    {img: `actors/heshamMaged.png`,
+    film: ["samerWeShaherWeBaher", "el7arbEl3alamiaElTalta", "BanatEl3am"]
+    },
+    {img: `actors/ahmedFahmi.png`,
+    film: ["samerWeShaherWeBaher", "el7arbEl3alamiaElTalta", "BanatEl3am", "kalbBalady", "lilaHanaWeSoror"]
+    },
+    {img: `actors/tal3atZakria.png`,
+    film: ["7a7aWeTofa7a", "AbuAli", "aboElAraby", "QessatAlhayAlSha3bi", "SayedElAtefy", "tabakhElRais", "3okal", "Ghapy", "KhaltyFaransa", "3awdaElNadla", "7aremKarem", "eo3aOeshk", "gaanaElBaianElTali", "HarameyaFeKG2", "HarameyaFeThailand"]
+    },
+    {img: `actors/tamerHosni.png`,
+    film: ["CaptainHema", "SayedElAtefy", "tesba73alaKhir", "Nour3iney", "OmarWeSalma2"]
+    },
+    {img: `actors/youssefEid.png`,
+    film: ["el7arbEl3alamiaElTalta", "elNazer", "ga3latnyMogrman", "H-daboor", "SayeBahr", "7a7aWeTofa7a", "ramadanMabrok", "el3yalHerbt", "EyalHabiba", "ElLemby8Giga", "TetaRaheba", "Aasef3alaElez3ag", "amirElBi7ar", "faselWaNa3od", "SarkhetNamla", "55Esaaf", "HelmAziz"]
+    },
+    {img: `actors/zina.png`,
+    film: ["Boshkash", "CaptainHema", "elshab7", "ElMaslaha", "SayedElAtefy", "ShebhMonharef", "AlGezira"]
+    },
+    {img: `actors/7asanAbdElFata7.png`,
+    film: ["samerWeShaherWeBaher", "aeathono", "7a7aWeTofa7a", "samerAboElNile", "ramadanMabrok", "amirElBi7ar", "zakiShan", "SHIKAMARA", "s3ediFeElGam3aElamirkia", "zarfTarek", "3esabaDr.Omar", "7aremKarem", "aboShanab", "Boshkash", "WahidSaidi", "katkot"]
+    },
+    {img: `actors/karemMahmmodAbdeElAziz.png`,
+    film: ["ziko", "elNazer", "Sa3aWaNos", "HasalKher", "ItlauliBarrah"]
+    },
+    {img: `actors/mohammedEmam.png`,
+    film: ["Sa3aWaNos", "3amohm", "kabtenMasr", "AboNasab", "HelmAziz", "ka7imFeElHend", "lilaHanaWeSoror", "LeesBaghdad"]
+    },
+    {img: `actors/ramezGalal.png`,
+    film: ["MedoMashakel", "ElBashaTelmeez", "a7lamElfataElataaesh", "EyalHabiba", "ShebhMonharef", "55Esaaf", "3eshElZaogia", "sab3ElBromba"]
+    },
+    {img: `actors/same77esin.png`,
+    film: ["elDadaDody", "H-daboor", "QessatAlhayAlSha3bi", "tabakhElRais", "FilmHendi", "eo3aOeshk", "gaanaElBaianElTali", "SahibSahbu"]
+    },
+    {img: `actors/ahmedRezk.png`,
+    film: ["eo3aOeshk", "Lakhmetrass", "3alaGanbYasta", "HelmAziz", "LeesBaghdad"]
+    },
+    {img: `actors/fat7iAbdElwehab.png`,
+    film: ["s3ediFeElGam3aElamirkia", "Sa3aWaNos", "3awdaElNadla", "LeesBaghdad"]
+    },
+    {img: `actors/hananTurk.png`,
+    film: ["HarameyaFeThailand", "HarameyaFeKG2", "IbrahimElAbyad", "gaanaElBaianElTali", "ElMaslaha"]
+    },
+    {img: `actors/khaledSar7an.png`,
+    film: ["3esabaDr.Omar", "7aremKarem", "kabtenMasr", "TetaRaheba", "xLarg", "EbnElQonsol"]
+    },
+    {img: `actors/mohammedRakab.png`,
+    film: ["tumnTastaAshrar", "ElBashaTelmeez", "kabtenMasr", "HarameyaFeKG2"]
+    },
+    {img: `actors/mohammedSharaf.png`,
+    film: ["elrahina", "Aasef3alaElez3ag", "zarfTarek", "zakiShan", "ShebhMonharef", "elDadaDody", "ga3latnyMogrman", "3esabaDr.Omar", "HarameyaFeThailand", "ramadanMabrok", "xLarg", "AskarFiElMu3askar", "QessatAlhayAlSha3bi"]
+    },
+    {img: `actors/shrefMoner.png`,
+    film: ["WeladEl3m", "kabtenMasr", "3anEl3eahkWeElHawa", "HelmAziz"]
+    },
+    {img: `actors/baiomyFoaad.png`,
+    film: ["wakfetRekala", "bankEl7az", "kabtenMasr", "3endamaYak3ElAnsan", "el7arbEl3alamiaElTalta", "aboShanab", "ItlauliBarrah", "ka7imFeElHend", "kalbBalady", "lilaHanaWeSoror", "LeesBaghdad", "BanatEl3am", "sab3ElBromba", "lafWeDaoaran", "sima3liBaba"]
+    },
+    {img: `actors/khaledSaleh.png`,
+    film: ["7arbItalya", "tumnTastaAshrar", "a7laElAuqat", "MedoMashakel", "3anEl3eahkWeElHawa", "EbnElQonsol", "AlGezira"]
+    },
+    {img: `actors/tota.png`,
+    film: ["ElLemby8Giga", "xLarg", "LeesBaghdad", "sab3ElBromba", "lilaHanaWeSoror", "sab3ElBromba"]
+    },
+    {img: `actors/ashrafAbdElBaki.png`,
+    film: ["Lakhmetrass", "tabakhElRais", "3amohm", "3alaGanbYasta", "ItlauliBarrah", "SahibSahbu"]
+    },
+
+]
---
+++ aflams.js
@@ -0,0 +1,320 @@
+let aflam = [
+    {img: `aflam/3okal.jpg`,
+    film: `3okal`
+    },
+    {img: `aflam/a7lamElfataElataaesh.jpg`,
+    film: `a7lamElfataElataaesh`
+    },
+    {img: `aflam/Aasef3alaElez3ag.jpg`,
+    film: `Aasef3alaElez3ag`
+    },
+    {img: `aflam/aeathono.jpg`,
+    film: `aeathono`
+    },
+    {img: `aflam/assalEsoed.jpg`,
+    film: `assalEsoed`
+    },
+    {img: `aflam/bo7a.jpg`,
+    film: `bo7a`
+    },
+    {img: `aflam/Boshkash.jpg`,
+    film: `Boshkash`
+    },
+    {img: `aflam/ElBashaTelmeez.jpg`,
+    film: `ElBashaTelmeez`
+    },
+    {img: `aflam/elLimbi.jpg`,
+    film: `elLimbi`
+    },
+    {img: `aflam/elNazer.jpg`,
+    film: `elNazer`
+    },
+    {img: `aflam/ElTalataYeshtghalonha.jpg`,
+    film: `ElTalataYeshtghalonha`
+    },
+    {img: `aflam/ga3latnyMogrman.jpg`,
+    film: `ga3latnyMogrman`
+    },
+    {img: `aflam/Ghapy.jpg`,
+    film: `Ghapy`
+    },
+    {img: `aflam/H-daboor.jpg`,
+    film: `H-daboor`
+    },
+    {img: `aflam/IbrahimElAbyad.jpg`,
+    film: `IbrahimElAbyad`
+    },
+    {img: `aflam/kedaReda.jpg`,
+    film: `kedaReda`
+    },
+    {img: `aflam/lafWeDaoaran.jpg`,
+    film: `lafWeDaoaran`
+    },
+    {img: `aflam/laTrago3WlaEstslam.jpg`,
+    film: `laTrago3WlaEstslam`
+    },
+    {img: `aflam/MedoMashakel.jpg`,
+    film: `MedoMashakel`
+    },
+    {img: `aflam/SayeBahr.jpg`,
+    film: `SayeBahr`
+    },
+    {img: `aflam/AskarFiElMu3askar.jpg`,
+    film: `AskarFiElMu3askar`
+    },
+    {img: `aflam/TetaRaheba.jpg`,
+    film: `TetaRaheba`
+    },
+    {img: `aflam/xLarg.jpg`,
+    film: `xLarg`
+    },
+    {img: `aflam/YaAnaYaKhalty.jpg`,
+    film: `YaAnaYaKhalty`
+    },
+    {img: `aflam/zakiShan.jpg`,
+    film: `zakiShan`
+    },
+    {img: `aflam/zarfTarek.jpg`,
+    film: `zarfTarek`
+    },
+    {img: `aflam/3amohm.jpg`,
+    film: `3amohm`
+    },
+    {img: `aflam/3endamaYak3ElAnsan.jpg`,
+    film: `3endamaYak3ElAnsan`
+    },
+    {img: `aflam/7a7aWeTofa7a.jpg`,
+    film: `7a7aWeTofa7a`
+    },
+    {img: `aflam/AbuAli.jpg`,
+    film: `AbuAli`
+    },
+    {img: `aflam/AlGezira.png`,
+    film: `AlGezira`
+    },
+    {img: `aflam/BanatEl3am.jpg`,
+    film: `BanatEl3am`
+    },
+    {img: `aflam/bankEl7az.jpg`,
+    film: `bankEl7az`
+    },
+    {img: `aflam/CaptainHema.jpg`,
+    film: `CaptainHema`
+    },
+    {img: `aflam/el3yalHerbt.jpg`,
+    film: `el3yalHerbt`
+    },
+    {img: `aflam/el7arbEl3alamiaElTalta.jpg`,
+    film: `el7arbEl3alamiaElTalta`
+    },
+    {img: `aflam/elDadaDody.jpg`,
+    film: `elDadaDody`
+    },
+    {img: `aflam/elFeelElAzrak.png`,
+    film: `elFeelElAzrak`
+    },
+    {img: `aflam/ElLemby8Giga.jpg`,
+    film: `ElLemby8Giga`
+    },
+    {img: `aflam/ElMaslaha.jpg`,
+    film: `ElMaslaha`
+    },
+    {img: `aflam/elrahina.jpg`,
+    film: `elrahina`
+    },
+    {img: `aflam/elshab7.jpg`,
+    film: `elshab7`
+    },
+    {img: `aflam/EyalHabiba.jpg`,
+    film: `EyalHabiba`
+    },
+    {img: `aflam/FeMahatetMasr.jpg`,
+    film: `FeMahatetMasr`
+    },
+    {img: `aflam/FilmHendi.png`,
+    film: `FilmHendi`
+    },
+    {img: `aflam/folElSinElAzim.jpg`,
+    film: `folElSinElAzim`
+    },
+    {img: `aflam/kabtenMasr.jpg`,
+    film: `kabtenMasr`
+    },
+    {img: `aflam/KalemnyShokran.jpg`,
+    film: `KalemnyShokran`
+    },
+    {img: `aflam/Karkar.jpg`,
+    film: `Karkar`
+    },
+    {img: `aflam/KhaltyFaransa.jpg`,
+    film: `KhaltyFaransa`
+    },
+    {img: `aflam/Ma3leshEhnaBenetbahdel.jpg`,
+    film: `Ma3leshEhnaBenetbahdel`
+    },
+    {img: `aflam/Nour3iney.jpg`,
+    film: `Nour3iney`
+    },
+    {img: `aflam/OmarWeSalma2.jpg`,
+    film: `OmarWeSalma2`
+    },
+    {img: `aflam/QessatAlhayAlSha3bi.jpg`,
+    film: `QessatAlhayAlSha3bi`
+    },
+    {img: `aflam/ramadanMabrok.jpg`,
+    film: `ramadanMabrok`
+    },
+    {img: `aflam/s3ediFeElGam3aElamirkia.jpg`,
+    film: `s3ediFeElGam3aElamirkia`
+    },
+    {img: `aflam/samerAboElNile.jpg`,
+    film: `samerAboElNile`
+    },
+    {img: `aflam/samerWeShaherWeBaher.jpg`,
+    film: `samerWeShaherWeBaher`
+    },
+    {img: `aflam/SarkhetNamla.jpg`,
+    film: `SarkhetNamla`
+    },
+    {img: `aflam/SayedElAtefy.jpg`,
+    film: `SayedElAtefy`
+    },
+    {img: `aflam/ShebhMonharef.jpg`,
+    film: `ShebhMonharef`
+    },
+    {img: `aflam/SHIKAMARA.jpg`,
+    film: `SHIKAMARA`
+    },
+    {img: `aflam/tabakhElRais.jpg`,
+    film: `tabakhElRais`
+    },
+    {img: `aflam/TaimourWeShafika.jpg`,
+    film: `TaimourWeShafika`
+    },
+    {img: `aflam/tesba73alaKhir.jpg`,
+    film: `tesba73alaKhir`
+    },
+    {img: `aflam/wakfetRekala.jpg`,
+    film: `wakfetRekala`
+    },
+    {img: `aflam/weladRezk.png`,
+    film: `weladRezk`
+    },
+    {img: `aflam/WeshEgram.jpg`,
+    film: `WeshEgram`
+    },
+    {img: `aflam/amirElBi7ar.jpg`,
+    film: `amirElBi7ar`
+    },
+    {img: `aflam/Sa3aWaNos.jpg`,
+    film: `Sa3aWaNos`
+    },
+    {img: `aflam/ziko.jpg`,
+    film: `ziko`
+    },
+    {img: `aflam/3andlibElDuki.jpg`,
+    film: `3andlibElDuki`
+    },
+    {img: `aflam/3awdaElNadla.jpg`,
+    film: `3awdaElNadla`
+    },
+    {img: `aflam/3esabaDr.Omar.jpg`,
+    film: `3esabaDr.Omar`
+    },
+    {img: `aflam/7arbItalya.jpg`,
+    film: `7arbItalya`
+    },
+    {img: `aflam/7aremKarem.jpg`,
+    film: `7aremKarem`
+    },
+    {img: `aflam/365YomSa3ada.jpg`,
+    film: `365YomSa3ada`
+    },
+    {img: `aflam/a7laElAuqat.jpg`,
+    film: `a7laElAuqat`
+    },
+    {img: `aflam/aboShanab.jpg`,
+    film: `aboShanab`
+    },
+    {img: `aflam/eo3aOeshk.jpg`,
+    film: `eo3aOeshk`
+    },
+    {img: `aflam/faselWaNa3od.jpg`,
+    film: `faselWaNa3od`
+    },
+    {img: `aflam/gaanaElBaianElTali.jpg`,
+    film: `gaanaElBaianElTali`
+    },
+    {img: `aflam/HarameyaFeKG2.jpg`,
+    film: `HarameyaFeKG2`
+    },
+    {img: `aflam/HarameyaFeThailand.jpg`,
+    film: `HarameyaFeThailand`
+    },
+    {img: `aflam/Lakhmetrass.jpg`,
+    film: `Lakhmetrass`
+    },
+    {img: `aflam/MatabSinai.jpg`,
+    film: `MatabSinai`
+    },
+    {img: `aflam/sima3liBaba.jpg`,
+    film: `sima3liBaba`
+    },
+    {img: `aflam/tumnTastaAshrar.jpg`,
+    film: `tumnTastaAshrar`
+    },
+    {img: `aflam/WeladEl3m.jpg`,
+    film: `WeladEl3m`
+    },
+    {img: `aflam/3alaGanbYasta.jpg`,
+    film: `3alaGanbYasta`
+    },
+    {img: `aflam/3anEl3eahkWeElHawa.jpg`,
+    film: `3anEl3eahkWeElHawa`
+    },
+    {img: `aflam/3eshElZaogia.jpg`,
+    film: `3eshElZaogia`
+    },
+    {img: `aflam/55Esaaf.jpg`,
+    film: `55Esaaf`
+    },
+    {img: `aflam/AboNasab.jpg`,
+    film: `AboNasab`
+    },
+    {img: `aflam/akherDikFeMasr.jpg`,
+    film: `akherDikFeMasr`
+    },
+    {img: `aflam/EbnElQonsol.jpg`,
+    film: `EbnElQonsol`
+    },
+    {img: `aflam/HasalKher.jpg`,
+    film: `HasalKher`
+    },
+    {img: `aflam/HelmAziz.jpg`,
+    film: `HelmAziz`
+    },
+    {img: `aflam/ItlauliBarrah.jpg`,
+    film: `ItlauliBarrah`
+    },
+    {img: `aflam/ka7imFeElHend.jpg`,
+    film: `ka7imFeElHend`
+    },
+    {img: `aflam/kalbBalady.jpg`,
+    film: `kalbBalady`
+    },
+    {img: `aflam/katkot.jpg`,
+    film: `katkot`
+    },
+    {img: `aflam/LeesBaghdad.jpg`,
+    film: `LeesBaghdad`
+    },
+    {img: `aflam/lilaHanaWeSoror.jpg`,
+    film: `lilaHanaWeSoror`
+    },
+    {img: `aflam/sab3ElBromba.jpg`,
+    film: `sab3ElBromba`
+    },
+    {img: `aflam/SahibSahbu.jpg`,
+    film: `SahibSahbu`
+    },
+]
